4ee46720743e4871127105805e97e3e9dc0dcc9f
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-05-25 11:04:45 -0700

    created function to get start and end dates in the correct format for each step

--- frontend/src/components/OverviewTable/OverviewTable.jsx
+++ frontend/src/components/OverviewTable/OverviewTable.jsx
@@ -5,10 +5,6 @@ import { Scheduler, MonthView, Appointments } from '@devexpress/dx-react-schedul
 
 const OverviewTable = ({data}) => {
 
-    const location = data.map((el) => {
-        return el.deployment.start_date
-    })
-
     const d = new Date()
     const day = d.getDate()
     const month = d.getMonth()+1
@@ -16,21 +12,20 @@ const OverviewTable = ({data}) => {
 
     const currentDate = `${year}-0${month}-${day}`;
     const schedulerData = [
-        { startDate: `${currentDate}T11:00`, endDate: `${data.startDate}T12:00`, title: 'Meeting' },
         { startDate: `${currentDate}T11:00`, endDate: `${currentDate}T12:00`, title: 'Meeting' },
-        { startDate: `${currentDate}T11:00`, endDate: `${currentDate}T12:00`, title: 'Meeting' },
-        { startDate: `${currentDate}T11:00`, endDate: `${currentDate}T12:00`, title: 'Meeting' },
+        { startDate: `${currentDate}T11:00`, endDate: `${currentDate}T12:00`, title: 'swimming' },
+        { startDate: `${currentDate}T11:00`, endDate: `${currentDate}T12:00`, title: 'swimming meeting' },
+        { startDate: `${currentDate}T11:00`, endDate: `${currentDate}T12:00`, title: 'Other' },
       ];
 
     return ( 
         <Paper>
-            {console.log(location)}
             <Scheduler
             data={schedulerData}
             >
             <ViewState
             />
-            <MonthView />
+            <MonthView intervalCount={2}/>
             <Appointments />
             </Scheduler>
         </Paper>
--- frontend/src/pages/DepHomePage/DepHomePage.jsx
+++ frontend/src/pages/DepHomePage/DepHomePage.jsx
@@ -11,9 +11,9 @@ import axios from "axios";
 const DepHomePage = () => {
   const [user, token] = useAuth();
   const [depPi, setDepPi] = useState({})
-  const [deployment, setDeployment] = useState({})
   const [steps, setSteps] = useState([])
 
+  //gets deployer personal information to display in the page
   const fetchDepPi = async () => {
     try {
       let response = await axios.get(`http://127.0.0.1:8000/api/deployers/${user.dep_id}/`)
@@ -23,44 +23,67 @@ const DepHomePage = () => {
     }
   }
 
-  const fetchDeployment = async () => {
+  //gets step information for deploying based on the deployment.
+  const fetchSteps = async () => {
     try {
-      let response = await axios.get(`http://127.0.0.1:8000/api/deployments/${depPi.deployment.id}/`)
-      setDeployment(response.data)
+      let response = await axios.get(`http://127.0.0.1:8000/api/steps/req/${depPi.deployment.id}/`)
+      setSteps(response.data)
     } catch (error) {
       console.log(error.message)
     }
   }
 
-  const fetchSteps = async () => {
-    try {
-      console.log(deployment)
-      let response = await axios.get(`http://127.0.0.1:8000/api/steps/req/1/`)
-      setSteps(response.data)
-    } catch (error) {
-      console.log(error.message)
+  const createStepTimeline = () => {
+    const today = new Date()
+    let startDates = []
+    let startDates2 = []
+    let endDates = []
+    console.log(steps)
+    let startDate = steps[0].requirement.requirement_list.deployment.start_date
+    let revSteps = []
+    for(let i=steps.length-1; i>(-1); i--){
+      revSteps.push(steps[i])
     }
-  }  
+    console.log(revSteps)
+    var n = Date.parse(startDate)
+    var d = new Date(n)
+    let e = new Date(d)
+    for(let i=0; i<revSteps.length; i++){
+      let revLength = revSteps[i].length
+      let startInMilli = d.setDate(d.getDate()-revLength)
+      let start = new Date(startInMilli)
+      let start2 = new Date(start)
+      startDates.push(start)
+      startDates2.push(start2)
+    }
+    endDates.push(e)
+    for(let i=0; i<revSteps.length-1; i++){
+      let endMinusOne = startDates2[i].setDate(startDates2[i].getDate()-1)
+      let newD = new Date(endMinusOne)
+      endDates.push(newD)
+    }
+    return({startDates, endDates})
+  }
+
+
+
 
   useEffect(() => {
     fetchDepPi()
   }, []);
 
   useEffect(async () => {
-    await fetchDeployment()
-  }, [depPi]);
-
-  useEffect(async () => {
     await fetchSteps()
-  }, [deployment]);
+    createStepTimeline()
+    console.log(createStepTimeline())
+  }, [depPi]);
 
 
   return (
     <div className="container">
-      {/* <OverviewTable/> */}
+      <OverviewTable/>
       <TaskCalendar/>
       <PIDisplay data={depPi}/>
-      {console.log(steps)}
     </div>
   );
 };
